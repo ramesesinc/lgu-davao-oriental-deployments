aff04f14804cef9d9e2f0af3cc514f83c0b01e79
worgflores <worgflores@gmail.com>
2021-01-09 16:51:26 +0800

    updated

---
+++ bin/appserver-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+RUN_DIR=`pwd`
+cd ..
+BASE_DIR=`pwd`
+
+docker system prune -f
+
+cd $BASE_DIR/appserver/epayment && docker-compose up -d
+
+cd $BASE_DIR/appserver/etracs && docker-compose up -d
+
+cd $RUN_DIR
